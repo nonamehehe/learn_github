398753c0992b9a94606a9f968b5f33706bb9f8f5
吕泷 <35415036+nonamehehe@users.noreply.github.com>
2020-09-14 20:12:01 +0800

    add

---
+++ 课堂手机使用率.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import sys
+sys.path.append(r'C:\Users\Administrator\Desktop\github\python-project')
+import openpyxl
+
+f = r'C:\Users\Administrator\Desktop\github\python-project\重庆公共运输职业学院课堂手机使用情况.xlsx'
+df = pd.read_excel(f)
